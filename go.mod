c58a264461c6028d8509583626a53c57c174c4dd
McMelonTV <mcmelontube@gmail.com>
2024-12-19 00:49:42 +0100

    some boilerplate go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NodiumHosting/VaultMapperSyncServer
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package vaultmappersyncserver
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
