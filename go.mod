be4b9947ff66803fc221f7719dc4d390a8d39124
user.name <user.email>
2024-12-19 08:31:17 +0100

    set up protobuf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NodiumHosting/VaultMapperSyncServer
 
 go 1.22.3
+
+require google.golang.org/protobuf v1.36.0 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/NodiumHosting/VaultMapperSyncServer/proto"
 )
 
 func main() {
+	vaultCell := proto.VaultCell{
+		X:        0,
+		z:        0,
+		celltype: proto.CellType_CELLTYPE_ROOM,
+	}
+
 	fmt.Println("Hello, World!")
 }
