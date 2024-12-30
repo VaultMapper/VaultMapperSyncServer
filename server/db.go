b7aaba85fe268b3d62a0924bb073970794faf615
McMelonTV <mcmelontube@gmail.com>
2024-12-30 07:26:22 +0100

    fix image loading

--- server/db.go
+++ server/db.go
@@ -11,8 +11,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	var err error
-	DB, err = gorm.Open(sqlite.Open("vaults.db"), &gorm.Config{})
+	DB, err := gorm.Open(sqlite.Open("vaults.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("failed to connect database")
 	}
