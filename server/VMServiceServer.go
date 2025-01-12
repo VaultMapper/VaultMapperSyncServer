993c6b6888192e7e2766aa3f07c71ddaad5d9c45
Anorak01 <adamfri007@gmail.com>
2025-01-12 02:50:24 +0100

    Started work on viewer handler

--- server/VMServiceServer.go
+++ server/VMServiceServer.go
@@ -12,8 +12,10 @@ import (
 var HUB = Hub{}
 
 var (
-	statsLimiter = rate.NewLimiter(5, 10) // 1 request per second with a burst of 5
-	statsMu      sync.Mutex
+	statsLimiter  = rate.NewLimiter(5, 10) // 1 request per second with a burst of 5
+	statsMu       sync.Mutex
+	viewerLimiter = rate.NewLimiter(5, 10) // 1 request per second with a burst of 5
+	viewerMu      sync.Mutex
 )
 
 func statsRateLimit(next http.Handler) http.Handler {
@@ -30,11 +32,26 @@ func statsRateLimit(next http.Handler) http.Handler {
 	})
 }
 
+func viewerRateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		viewerMu.Lock()
+		defer viewerMu.Unlock()
+
+		if !viewerLimiter.Allow() {
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func Run(ip string, port int) {
 	fmt.Println("HELLO FROM SERVER")
 
 	http.HandleFunc("/", handshakeHandler)
 	http.Handle("/stats", statsRateLimit(http.HandlerFunc(statsHandler)))
+	http.Handle("/view", viewerRateLimit(http.HandlerFunc(viewerHandshakeHandler)))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", ip, port), nil); err != nil {
 		log.Fatal(err)
 	}
